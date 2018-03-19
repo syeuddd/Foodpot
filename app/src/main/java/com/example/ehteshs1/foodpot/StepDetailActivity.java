41bb9207e33c5944a3d620a96c65bbf8948c83f0
starlord <ehteshamuddin.syed@gmail.com>
2018-03-19 13:17:44 -0600

    Fixed lifecycle bugs and also added feature to play videos for each step and it works fine - recommit

--- app/src/main/java/com/example/ehteshs1/foodpot/StepDetailActivity.java
+++ app/src/main/java/com/example/ehteshs1/foodpot/StepDetailActivity.java
@@ -2,35 +2,25 @@ package com.example.ehteshs1.foodpot;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ehteshs1.foodpot.model.Step;
 import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlaybackControlView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -45,9 +35,6 @@ public class StepDetailActivity extends AppCompatActivity {
     ArrayList<Step> currentStepList;
     int counter = 0;
     TextView description;
-
-
-
     SimpleExoPlayer player;
     BandwidthMeter bandwidthMeter;
     ExtractorsFactory extractorsFactory;
@@ -56,9 +43,9 @@ public class StepDetailActivity extends AppCompatActivity {
     DefaultBandwidthMeter defaultBandwidthMeter;
     DataSource.Factory dataSourceFactory;
     MediaSource mediaSource;
-    PlaybackControlView playbackControlView;
-    ProgressBar bar;
-
+    String videoUrl;
+    SimpleExoPlayerView recipeView;
+    TextView errorTextView;
 
 
     @Override
@@ -74,33 +61,38 @@ public class StepDetailActivity extends AppCompatActivity {
         description = findViewById(R.id.stepDescription);
         final Button nextButton = findViewById(R.id.nextStepButton);
         final Button previousButton = findViewById(R.id.previousStepButton);
-        bar = findViewById(R.id.progressBar);
+        errorTextView = findViewById(R.id.noVideoErrorTextView);
 
-        ImageButton playButton = findViewById(R.id.exo_play);
-        ImageButton pauseButton = findViewById(R.id.exo_pause);
+        // initialize exo player stuff
 
-        initializePlayer(currentStepList.get(counter).getVideoURL());
+        bandwidthMeter = new DefaultBandwidthMeter();
 
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                player.setPlayWhenReady(true);
-            }
-        });
+        extractorsFactory = new DefaultExtractorsFactory();
 
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                player.setPlayWhenReady(false);
+        trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
 
-            }
-        });
+        trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+
+        defaultBandwidthMeter = new DefaultBandwidthMeter();
+
+        dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this,"foodPot"),defaultBandwidthMeter);
+
+        player = ExoPlayerFactory.newSimpleInstance(this,trackSelector);
+
+        player.setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+
+        recipeView = findViewById(R.id.playerView);
+
+//        ImageButton playButton = findViewById(R.id.exo_play);
+//        ImageButton pauseButton = findViewById(R.id.exo_pause);
 
 
         if (currentStepList != null){
             Step currentStep = currentStepList.get(counter);
             description.setText(currentStep.getDescription());
+            changeVideo(currentStepList.get(counter).getVideoURL());
         }
+
 
         if (counter==0){
             previousButton.setVisibility(View.INVISIBLE);
@@ -122,6 +114,9 @@ public class StepDetailActivity extends AppCompatActivity {
                             previousButton.setVisibility(View.VISIBLE);
                         }
                         StepDetailActivity.this.description.setText(currentStepList.get(counter).getDescription());
+                        player.setPlayWhenReady(false);
+                        changeVideo(currentStepList.get(counter).getVideoURL());
+
                     } else {
                         nextButton.setVisibility(View.INVISIBLE);
                     }
@@ -143,6 +138,8 @@ public class StepDetailActivity extends AppCompatActivity {
 
                     if (counter < currentStepList.size()-1) {
                         StepDetailActivity.this.description.setText(currentStepList.get(counter).getDescription());
+                        player.setPlayWhenReady(false);
+                        changeVideo(currentStepList.get(counter).getVideoURL());
                         if (nextButton.getVisibility()==View.INVISIBLE){
                             nextButton.setVisibility(View.VISIBLE);
                         }
@@ -155,83 +152,62 @@ public class StepDetailActivity extends AppCompatActivity {
 
     }
 
-    private void initializePlayer(String url) {
+    private void changeVideo(String url) {
 
-        bandwidthMeter = new DefaultBandwidthMeter();
+        if (!url.isEmpty() && player !=null && recipeView !=null){
 
-        extractorsFactory = new DefaultExtractorsFactory();
+            mediaSource = new ExtractorMediaSource(Uri.parse(url),dataSourceFactory,extractorsFactory,null,null);
 
-        trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            player.prepare(mediaSource);
+            player.setPlayWhenReady(true);
 
-        trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+            recipeView.requestFocus();
+            recipeView.setPlayer(player);
 
-        defaultBandwidthMeter = new DefaultBandwidthMeter();
+            recipeView.setVisibility(View.VISIBLE);
+            errorTextView.setVisibility(View.GONE);
 
-        dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this,"foodPot"),defaultBandwidthMeter);
+        }else {
 
-        mediaSource = new ExtractorMediaSource(Uri.parse(url),dataSourceFactory,extractorsFactory,null,null);
-
-        player = ExoPlayerFactory.newSimpleInstance(this,trackSelector);
-
-        player.prepare(mediaSource);
-
-        //player.setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-
-        player.addListener(new Player.EventListener() {
-            @Override
-            public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-            }
-
-            @Override
-            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-            }
-
-            @Override
-            public void onLoadingChanged(boolean isLoading) {
-
-            }
-
-            @Override
-            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-
-            }
-
-            @Override
-            public void onRepeatModeChanged(int repeatMode) {
-
-            }
-
-            @Override
-            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
-
-            }
-
-            @Override
-            public void onPlayerError(ExoPlaybackException error) {
-
-            }
-
-            @Override
-            public void onPositionDiscontinuity(int reason) {
-
-            }
-
-            @Override
-            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-            }
-
-            @Override
-            public void onSeekProcessed() {
-
-            }
-        });
-        SimpleExoPlayerView recipeView = findViewById(R.id.playerView);
-        recipeView.requestFocus();
-        recipeView.setPlayer(player);
+            recipeView.setVisibility(View.GONE);
+            errorTextView.setVisibility(View.VISIBLE);
+        }
 
     }
+
+
+    private void releasePlayer(){
+        if (player!=null){
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.setPlayWhenReady(true);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void test(){
+        
+    }
 }
+
 
